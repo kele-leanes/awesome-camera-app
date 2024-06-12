ca6965bc23e708ac1072d75471e485765bcc46ec
Ezequiel Leanes <ezequiel.leanes@gmail.com>
2024-06-12 18:33:55 -0300

    feat: Add Colors and Dimensions constants

---
+++ src/constants/Colors.ts
@@ -0,0 +1,11 @@
+const colors = {
+  background: '#1A1A1A',
+  buttons: '#007AFF',
+  secondaryElements: '#D1D1D6',
+  highlights: '#FFCC00',
+  success: '#34C759',
+  error: '#FF3B30',
+  text: '#FFFFFF',
+};
+
+export default colors;
---
+++ src/constants/Dimensions.ts
@@ -0,0 +1,8 @@
+import { Dimensions as RNDimensions } from 'react-native';
+
+const { width, height } = RNDimensions.get('window');
+
+export default {
+  width,
+  height,
+};
